f35efee640ebdb9b8deccf1eefd198c2ef33825b
exlntz <chempionat34@gmail.com>
2024-12-23 19:10:59 +0300

    vitya

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import pandas as pd
-print("name")
-print(0)
-sdprint('Chto-to tam')
-adssadkjsa
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/atm')  #Automated teller machine
+def atm():
+    return render_template('atm.html')
+
+
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
