c5831a0f3dcff0de067fbc4e34cb3b00e9b61b35
Ivan <warok0606@gmail.com>
2024-12-25 21:12:48 +0300

    VanyaErrorsCount

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 import pandas as pd
-data=pd.read_csv('telemi_events.csv')
-print(data)
+import datetime
+#вывод за неделю не работает,пофикшу позже
+result = pd.read_csv("telemi_events.csv")
+eventtype=result['EventType'].tolist()
+time=result['Timestamp'].tolist()
+firsttime=list(map(int,time[0][0:10].split('-')))
+
+startdate=datetime.date(firsttime[0],firsttime[1],firsttime[2])
+print(startdate.weekday())
+for i in range(0,len(time),7):
+    print("Ошибки за неделю")
+    for item in eventtype[i:i+7]:
+        if "ОШИБКА" in item.upper():
+             print(item, time[(eventtype.index(item))])
