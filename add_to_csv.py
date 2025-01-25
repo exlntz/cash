4743acf16c924cd7b9b877d02792ca2590fe50d5
exlntz <chempionat34@gmail.com>
2025-01-25 18:02:27 +0300

    yaustal

--- add_to_csv.py
+++ add_to_csv.py
@@ -2,20 +2,25 @@ import csv
 
 
 def add_mechanic(text):
+    processed_text=text.split()
     with open('csvfiles/mechanics.csv', 'a', encoding='utf-8', newline='') as fm:
-        csv.writer(fm).writerow(text)
+        csv.writer(fm).writerow(processed_text)
 
 
 def add_car(text):
     with open('csvfiles/cars.csv', 'a', encoding='utf-8', newline='') as fc:
         csv.writer(fc).writerow(text)
 
-#строку с машиной и с механиком нужно брать из админ панели
-#желательно сделать кнопку добавить мех/машину и там уже сделать строку ввода
-row_car=[input()]
-row_mechanic=[input()]
+
+
+print('''Введите данные механика через пробел в таком формате:
+
+                Имя Возраст Пол''')
+row_mechanic=input()#строку с машиной и с механиком нужно брать из админ панели
+#row_car=[input()]    #желательно сделать кнопку добавить мех/машину и там уже сделать строку ввода
 
 
 add_mechanic(row_mechanic)
-add_car(row_car)
+#add_car(row_car)
 
+#потом доделаю
