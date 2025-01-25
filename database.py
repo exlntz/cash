b5e7e04949d4d51d01e4a14d168b83b5ae256e20
Ivan <warok0606@gmail.com>
2025-01-25 16:45:34 +0300

    VanyaJsonAtm2

--- database.py
+++ database.py
@@ -37,7 +37,26 @@ k=0
 #     print(s, s[8:10],k)
 #     k+=1
 
-
+all_value=set(value)
+all_errors=[]
+for status in all_value:
+    if str(status).isdigit()==0:
+        if len(str(status).split())>1:
+            all_errors.append(status)
+all_errors.sort()
+non_critical_errors=[]
+critical_errors=[]
+non_critical_errors.extend(all_errors[20:23])
+non_critical_errors.extend(all_errors[24:30])
+non_critical_errors.extend(all_errors[31:33])
+non_critical_errors.extend(all_errors[35:39])
+critical_errors.extend((all_errors)[2:10])
+critical_errors.extend(all_errors[11:19])
+critical_errors.append(all_errors[33])
+critical_errors.append(all_errors[39])
+print(non_critical_errors)
+print(critical_errors)
+print(all_errors)
 
 
 print(weekindexcount(338,time, int(start_date.weekday())))
@@ -50,21 +69,19 @@ for i in event_type:
         last=lastcount
         errorlst.append(i)
     lastcount+=1
-print(set(errorlst))
+# print(set(errorlst))
 
 k=0
 week=1
+with open('bankomat_data.json', 'w', encoding='utf-8') as file:
+    file.write('')
 while k<lastcount:
-    print("------   ------\n-----------\n----------Ошибки за неделю---------------\n----------------------\n---------------------------")
     stopcount=weekindexcount(k,time, start_date.weekday())
     weekdata= {}
     for item in event_type[k: stopcount]:
         if k>=stopcount:
             break
         if "ОШИБКА" in item.upper():
-            #print(event_type[k], time[k], AtmID[k], k,)
-
-            print({AtmID[k]:event_type[k]})
             weekdata[AtmID[k]]=event_type[k]
             firsttime = list(map(int, time[k][0:10].split('-')))
         k+=1
@@ -72,3 +89,35 @@ while k<lastcount:
         file.write(json.dumps({('неделя '+str(week)):weekdata}, indent=4, ensure_ascii=False))
     start_date=datetime.date(firsttime[0],firsttime[1],firsttime[2])
     week+=1
+#print(all_errors)
+repair_needs={}
+# critical_errorsDATA={}
+# errorsDATA={}
+# non_errorsDATA={}
+critical_errorsLIST=[]
+errorsLIST=[]
+non_errorsLIST=[]
+index=0
+#r
+for event in value:
+    deltaDATA={}
+    if event in all_errors:
+        deltaDATA['id'] = AtmID[index]
+        deltaDATA['date'] = time[index]
+        deltaDATA['error'] = value[index]
+        if value[index] in critical_errors:
+            critical_errorsLIST.append(deltaDATA)
+
+        elif value[index] in non_critical_errors:
+            errorsLIST.append(deltaDATA)
+        else:
+            non_errorsLIST.append(deltaDATA)
+    index+=1
+# print(errorsLIST)
+# print(non_errorsLIST)
+# print(critical_errorsLIST)
+repair_needs['critical_errors']=critical_errorsLIST
+repair_needs['errors']=errorsLIST
+repair_needs['non_errors']=non_errorsLIST
+with open('atmdata2.json', 'w', encoding='utf-8') as file:
+    file.write(json.dumps(repair_needs, indent=4, ensure_ascii=False))
